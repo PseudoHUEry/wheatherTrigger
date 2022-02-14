a21b477a3464b01f8127f58c19666552470468a5
Harry Raddatz <85199461+PseudoHUEry@users.noreply.github.com>
2022-02-14 17:03:20 -0300

    fix

--- bd/index.js
+++ bd/index.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose')
 const env = require('../helper/env')
 
 module.exports = async() => {
-  return await mongoose.connect(env[`${process.env.NODE_ENV}`] || "mongodb://127.0.0.1:27017/weather")
+  return await mongoose.connect(env[`${process.env.NODE_ENV}`] || "mongodb://db:27017/weather")
 }
