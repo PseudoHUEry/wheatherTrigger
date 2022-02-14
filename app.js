80d6fedda666b3df2facaadf4b84f8c4ab2c5d2a
Harry Raddatz <85199461+PseudoHUEry@users.noreply.github.com>
2022-02-14 13:00:56 -0300

    fix

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-require('dotenv/config')
-const moment = require('moment-timezone')
-const climaTempo = require('./helper/requestClimatempo')
-const app = require('express')()
-const startDatabase = require('./bd/index')
-console.log(process.cwd() + '/.env')
-moment.tz.setDefault('America/Sao_Paulo')
-setTimeout(() => {
-    climaTempo()
-}, 1000)
-app.use('/', (req,res) =>{
-    res.send('health')
-})
+require("dotenv/config");
+const moment = require("moment-timezone");
+const climaTempo = require("./helper/requestClimatempo");
+const app = require("express")();
+const startDatabase = require("./bd/index");
+moment.tz.setDefault("America/Sao_Paulo");
 
-app.listen(process.env.PORT || 3000, ()=>{
-    startDatabase().then(x => console.log("Banco de Dados, Conectado"))
-    console.log("Servidor online!")
-})
+app.use("/", (req, res) => {
+  res.send("health");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  startDatabase().then((x) => {
+    setTimeout(() => {
+      climaTempo();
+    }, 1000);
+    console.log("Banco de Dados, Conectado");
+  });
+  console.log(`Servidor online! PORTA: ${process.env.PORT}`);
+});
