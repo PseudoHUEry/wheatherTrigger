f61bc23d1f062086074c2fb4a1d7c022b02e1cfb
Harry Raddatz <85199461+PseudoHUEry@users.noreply.github.com>
2022-02-14 10:47:24 -0300

    fix

--- bd/index.js
+++ bd/index.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose')
 const env = require('../helper/env')
 
 module.exports = async() => {
-  return await mongoose.connect(env[`${process.env.NODE_ENV}`])
+  return await mongoose.connect(env[`${process.env.NODE_ENV}`]||"mongodb://localhost:27017/wheater")
 }
